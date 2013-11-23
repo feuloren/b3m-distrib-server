1a49942ffe50e7ce843425c18da0dda2795179b7
Florent Thévenet <florent@fthevenet.fr>
2013-11-23 23:53:11 +0100

    [Formatage du code] Construction de la recherche sur plusieurs lignes

--- app/handlers/recherche.py
+++ app/handlers/recherche.py
@@ -10,5 +10,8 @@ class RechercheHandler(BaseHandler):
         if term is None:
             return {'commandes' : None}
         else:
-            results = self.db.query(Commande).filter(Commande.nom.like('%'+term+'%') | Commande.prenom.like('%'+term+'%') | Commande.mail.like('%'+term+'%')).limit(20)
+            results = self.db.query(Commande).\
+                join(Commande.tarif).\
+                filter(Commande.nom.like('%'+term+'%') | Commande.prenom.like('%'+term+'%') | Commande.mail.like('%'+term+'%')).\
+                limit(20)
             return {'commandes' : (commande.info_summary for commande in results)}
